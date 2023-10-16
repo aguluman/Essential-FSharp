7b03b3e4b50fba67e3bbed22d0ec4c3c53502ed5
Chukwuma Akunyili <chukwumaakunyili@gmail.com>
2023-10-16 13:30:38 +0100

    add: chapter 10

---
+++ Chapter 10/FizzBuzz.fsx
@@ -0,0 +1,30 @@
+﻿// This is a class type in fsharp
+type FizzBuzz() =
+    member _.Calculate(value) =
+        [ (3, "Fizz"); (5, "Buzz") ]
+        |> List.map (fun (v, s) -> if value % v = 0 then s else "")
+        |> List.reduce (+)
+        |> fun s -> if s = "" then string value else s
+
+//To instantiate the class type we do.
+let fizzBuzz = FizzBuzz()
+let fifteen = fizzBuzz.Calculate(15)
+let fifteenPA = 15 |> fizzBuzz.Calculate
+
+//This is a function that uses the new FizzBuzz class type
+let doFizzBuzz range =
+    let fizzBuzz = FizzBuzz()
+    //range |> List.map(fun n -> fizzBuzz.Calculate(n))not simple enough
+    range |> List.map fizzBuzz.Calculate //simple enough
+
+let output = doFizzBuzz [1..15]
+
+//Instead of hard-coding the mapping, we can pass it in through the default constructor
+type FizzBuzzTwo(mapping) =
+    let calculate n =
+        mapping
+        |> List.map (fun (v, s) -> if n % v = 0 then s else "")
+        |> List.reduce (+)
+        |> fun s -> if s = "" then string n else s
+
+    member _.Calculate(value) = calculate value
---
+++ Chapter 10/FizzBuzzInterface.fsx
@@ -0,0 +1,19 @@
+﻿//The interface
+type IFizzBuzz =
+    abstract member Calculate : int -> string
+
+type FizzBuzzTwo(mapping) =
+    let calculate n =
+        mapping
+        |> List.map (fun (v, s) -> if n % v = 0 then s else "")
+        |> List.reduce (+)
+        |> fun s -> if s <> "" then string n else s
+
+    interface IFizzBuzz with
+        member _.Calculate(value) = calculate value
+
+let doFizzBuzz range =
+    let fizzBuzz = FizzBuzzTwo(mapping = [ 3, "Fizz"; 5, "Buzz" ])
+    range |> List.map (fizzBuzz :> IFizzBuzz).Calculate
+
+let output = doFizzBuzz [ 1..15 ]
